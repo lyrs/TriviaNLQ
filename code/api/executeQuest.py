1be363e87073907be58329b20ce41ed5cda52752
Muhammad Shahzib <muhammad.shahzaib@outlook.com>
2023-01-27 14:50:23 +0100

    speedup web

--- code/api/executeQuest.py
+++ code/api/executeQuest.py
@@ -10,9 +10,36 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import generator_utils as utils
 import subprocess
 import sys  
+from fairseq.models.transformer import TransformerModel
 
+trans = TransformerModel.from_pretrained(
+  'output/models/',
+  checkpoint_file='checkpoint_best.pt',
+  data_name_or_path='data/monument_600/fairseq-data-bin',
+  is_gpu=True
+).cuda()
 
-def predict_sparql(question):
+def predict_sparqltemp_sparql(question): 
+    queryEnc = trans.translate(question)
+    query = utils.decode(queryEnc)
+
+    try:
+        results = None
+        error = None
+        sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+        sparql.setReturnFormat(JSON)
+
+        sparql.setQuery(query)  # the previous query as a literal string
+        results = sparql.query().convert()
+    except Exception as e:
+        print('Failed Error: '+ str(e))
+        error=  str(e)
+    return {"queryEncoding": queryEnc,
+            "sparql": query,
+            "dbpediaResult": results,
+            "error": error}
+
+def temp_sparql(question):
     with open("source.txt", "w") as text_file:
         # Write the input to a file
         text_file.write("%s" % question)
