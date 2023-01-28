711256de95b0c69b5d310de9fe6de77ebcf1f889
Rohaam-ROSHAN <91160029+Rohaam-ROSHAN@users.noreply.github.com>
2023-01-28 14:02:41 +0100

    duplicate existing test_fairseq to fconve, edit it

--- training/script/test_fairseq.sh
+++ training/script/test_fairseq.sh
@@ -1,5 +1,5 @@
 DDIR=data/monument_600
-MDIR=output/models/fairseq_fconv_wmt_en_de
+MDIR=output/models
 RDIR=results/result
 
 if [ -n "$1" ]
@@ -18,12 +18,12 @@ then
     fi
 fi
 
-fairseq-generate $DDIR/fairseq-data-bin \
---gen-subset train \
---path $MDIR/checkpoint_last.pt \
---beam 5 > $RDIR/train_output.txt
+# fairseq-generate $DDIR/fairseq-data-bin \
+# --gen-subset train \
+# --path $MDIR/checkpoint_last.pt \
+# --beam 5 > $RDIR/train_output.txt
 
-python3 fairseq_output_reader.py $RDIR/train_output.txt > $RDIR/train_translation.sparql
+# python3 fairseq_output_reader.py $RDIR/train_output.txt > $RDIR/train_translation.sparql
 
 fairseq-generate $DDIR/fairseq-data-bin \
 --gen-subset valid \
@@ -32,12 +32,12 @@ fairseq-generate $DDIR/fairseq-data-bin \
 
 python3 fairseq_output_reader.py $RDIR/dev_output.txt > $RDIR/dev_translation.sparql
 
-# fairseq-generate $DDIR/fairseq-data-bin \
-# --gen-subset test \
-# --path $MDIR/checkpoint_best.pt \
-# --beam 5 > $RDIR/test_output.txt
+fairseq-generate $DDIR/fairseq-data-bin \
+--gen-subset test \
+--path $MDIR/checkpoint_best.pt \
+--beam 5 > $RDIR/test_output.txt
 
-# python3 fairseq_output_reader.py $RDIR/test_output.txt > $RDIR/test_translation.sparql
+python3 fairseq_output_reader.py $RDIR/test_output.txt > $RDIR/test_translation.sparql
 
 # Query and Analyze
 # python3 generate.py $DDIR $RDIR
